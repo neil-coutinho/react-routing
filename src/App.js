1219f7e10727f225728e735955fc25c426309377
Neil Coutinho <njcoutinho@gmail.com>
2021-01-17 09:34:30 +0530

    Using Router and React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Task from "./tasks/Task";
+import React from "react";
+import { BrowserRouter as Router,  } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+     
+        <p>Hello from App</p>
+        <Task />
+      
+    </React.Fragment>
+    
+   
+    // <Router>
+    //   <p>Hello from App</p>
+    //   <Task />
+    // </Router>
+ 
   );
 }
 
---
+++ src/tasks/Task.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+class Task extends React.Component {
+
+    render() {
+        return <p>Hello From Task</p>
+    }
+
+}
+
+export default Task;
